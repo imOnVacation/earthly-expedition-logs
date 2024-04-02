49a8ecdb2f8857dbb52a6438c3c0bb5b0c9ef4d4
Leshi Chen <leshi@pdx.edu>
2024-04-02 10:56:27 -0700

    plug in flowbite to tailwindcss

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,3 @@
-import flowbitePlugin from 'flowbite/plugin';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -10,5 +8,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [flowbitePlugin],
+  plugins: [require('flowbite/plugin')],
 };
