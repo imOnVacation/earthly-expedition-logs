7bd827d80114e3d8e11bbb4aab6c6529d1c4c840
Leshi Chen <leshi@pdx.edu>
2024-04-05 20:30:31 -0700

    create and add theme provider in main

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import { Button, Navbar, TextInput, Dropdown, Avatar } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
+import { MdDarkMode, MdOutlineLightMode } from 'react-icons/md';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice';
 
 export default function Header() {
   const path = useLocation().pathname;
   const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const theme = useSelector((state) => state.theme.theme);
 
   return (
     <Navbar className='border-b-2'>
@@ -30,9 +33,14 @@ export default function Header() {
       <Button className='w-12 h-10 lg:hidden' color='gray' pill>
         <AiOutlineSearch />
       </Button>
-      <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:incline' color='gray' pill>
-          <FaMoon />
+      <div className='flex gap-5 md:order-2'>
+        <Button
+          className='w-12 h-10 hidden sm:inline'
+          color='gray'
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === 'light' ? <MdOutlineLightMode /> : <MdDarkMode />}
         </Button>
         {currentUser ? (
           <Dropdown
@@ -63,7 +71,6 @@ export default function Header() {
             </Button>
           </Link>
         )}
-
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
