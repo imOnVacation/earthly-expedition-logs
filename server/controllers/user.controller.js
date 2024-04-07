e0a994858a8b3511b141c04f6146f8b7899f982b
Leshi Chen <leshi@pdx.edu>
2024-04-07 14:49:18 -0700

    fix: return using error handle

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -54,7 +54,7 @@ export const updateUser = async (req, res, next) => {
     );
     const { password, ...rest } = updatedUser._doc;
     res.status(200).json(rest);
-  } catch (err) {
-    return next(errorHandler(500, err.message));
+  } catch (error) {
+    next(error);
   }
 };
