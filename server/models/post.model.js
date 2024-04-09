df2db5c90b476483feba42f7d29f8f217026e8fe
Leshi Chen <leshi@pdx.edu>
2024-04-09 16:08:22 -0700

    add: complete create a create_post post request in the backend api route

---
+++ server/models/post.model.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:
+        'https://lirp.cdn-website.com/405c938e/dms3rep/multi/opt/blank_image_2-640w.jpg',
+    },
+    category: {
+      type: String,
+      default: 'uncategorized',
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
