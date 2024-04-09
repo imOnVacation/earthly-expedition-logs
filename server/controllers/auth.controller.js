9f65a9f36adbbdc2f81a11665d5121f7a1346c95
Leshi Chen <leshi@pdx.edu>
2024-04-09 15:10:44 -0700

    add: add admin functionality to the user

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -49,7 +49,10 @@ export const signin = async (req, res, next) => {
 
     const { password: pass, ...rest } = validUser._doc;
 
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.password },
+      process.env.JWT_SECRET
+    );
     res
       .status(200)
       .cookie('access_token', token, {
@@ -68,7 +71,10 @@ export const google = async (req, res, next) => {
     const user = await User.findOne({ email });
 
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password, ...rest } = user._doc;
       return res
         .status(200)
@@ -90,7 +96,10 @@ export const google = async (req, res, next) => {
         profilePicture: googlePhotoUrl,
       });
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: newUser._id, isAdmin: newUser.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password, ...rest } = newUser._doc;
       res
         .status(200)
