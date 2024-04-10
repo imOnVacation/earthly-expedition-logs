8e9d2a0b1a164c7272c02363d25739d74e377de5
Leshi Chen <leshi@pdx.edu>
2024-04-09 19:31:49 -0700

    fix: typo on the file name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import Projects from './pages/Projects';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
-import OnlyAdminPrivateRoute from './components/onlyAdminPrivateRoute';
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
 
 export default function App() {
