0bac0ecb4c409e38951fc352858ab59a8bf4f9b1
Leshi Chen <leshi@pdx.edu>
2024-04-16 10:36:10 -0700

    add: dashboard overview component to the admin dashboard and dashside bar

--- client/src/components/DashSideBar.jsx
+++ client/src/components/DashSideBar.jsx
@@ -5,6 +5,7 @@ import {
   HiDocumentText,
   HiOutlineUserGroup,
   HiUser,
+  HiChartPie,
 } from 'react-icons/hi';
 import { useState, useEffect } from 'react';
 import { useLocation, Link } from 'react-router-dom';
@@ -45,6 +46,17 @@ export default function DashSideBar() {
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
         <Sidebar.ItemGroup className='flex flex-col gap-1'>
+          {currentUser && currentUser.isAdmin && (
+            <Link to='/dashboard?tab=dash'>
+              <Sidebar.Item
+                active={tab === 'dash' || !tab}
+                icon={HiChartPie}
+                as='div'
+              >
+                Dashboard
+              </Sidebar.Item>
+            </Link>
+          )}
           <Link to='/dashboard?tab=profile'>
             <Sidebar.Item
               active={tab === 'profile'}
