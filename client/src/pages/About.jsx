193fab4ca4b17520e1f0bd1f5cd33d85486cbedb
Leshi Chen <leshi@pdx.edu>
2024-04-17 12:43:42 -0700

    add: styling on about page; need to complete the content later

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,16 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Leshi's Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>Placeholder... complete it later...</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
