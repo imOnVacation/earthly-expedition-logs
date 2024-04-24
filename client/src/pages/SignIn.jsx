f6ccf9ad3bf8146fa60baeedf14d8aab1f9173c3
Leshi Chen <leshi@pdx.edu>
2024-04-24 13:49:09 -0700

    change style on sign in and signup pages

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -54,12 +54,7 @@ export default function SignIn() {
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
         {/* left */}
         <div className='flex-1'>
-          <Link to='/' className='font-bold dark:text-white text-4xl'>
-            <span className='px-2 py-1 bg-gradient-to-r from-green-400 to-blue-400 rounded-lg text-white'>
-              Leshi&apos;s
-            </span>
-            Blog
-          </Link>
+          <span className='px-7 py-1 text-9xl'>&#127758;</span>
           <p className='text-sm mt-5'>
             This is a demo project. You can sign in with your email and password
             or with Google.
