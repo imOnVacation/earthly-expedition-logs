504ec277af0951734e8b7b64ec816848b1002867
Leshi Chen <leshi@pdx.edu>
2024-04-24 13:12:04 -0700

    add paragraph to placeholder in about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,10 +4,14 @@ export default function About() {
       <div className='max-w-2xl mx-auto p-3 text-center'>
         <div>
           <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Leshi's Blog
+            About Earthly Expedition Logs
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
-            <p>Placeholder... complete it later...</p>
+            <p>
+              I consider myself a traveler on this planet, and this is where I
+              record every little experience I have had here. Feel free to
+              browse around and leave comments.
+            </p>
           </div>
         </div>
       </div>
